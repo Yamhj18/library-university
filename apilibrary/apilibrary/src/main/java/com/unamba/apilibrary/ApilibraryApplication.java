4683d02f73a7ab3e3f419899fd246138f778364a
Sr.Yamil <232170@unamba.edu.pe>
2026-07-22 12:04:47 -0500

    fix(security): remove hardcoded admin password overwrite in DataLoader

--- apilibrary/apilibrary/src/main/java/com/unamba/apilibrary/ApilibraryApplication.java
+++ apilibrary/apilibrary/src/main/java/com/unamba/apilibrary/ApilibraryApplication.java
@@ -2,10 +2,6 @@ package com.unamba.apilibrary;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
-import com.unamba.apilibrary.repository.RepositoryUser;
-import com.unamba.apilibrary.entity.EntityUser;
 
 @SpringBootApplication
 public class ApilibraryApplication {
@@ -13,15 +9,4 @@ public class ApilibraryApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApilibraryApplication.class, args);
 	}
-
-    @Bean
-    public CommandLineRunner dataLoader(RepositoryUser repo) {
-        return args -> {
-            EntityUser admin = repo.findByEmail("admin@unamba.edu.pe").orElse(null);
-            if (admin != null) {
-                admin.setPassword("$2a$10$r40efuVFIhb9fb4F98VMlOY.TE9ypDXhX9Ge.N/kMolfhNDhrlgLO");
-                repo.save(admin);
-            }
-        };
-    }
 }
